60337b490297c017245482b94a3d99a3faffc76f
Jacob Gomez <jakegomez814@hotmail.com>
2022-10-13 09:14:33 -0400

    Add project files.

---
+++ Bug Squasher Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bug_Squasher_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
